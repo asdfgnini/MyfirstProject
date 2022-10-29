d13fc0b74be0fe081f7cce853520e82b651f0e02
lianjinsgushi <63616108+asdfgnini@users.noreply.github.com>
2022-10-29 19:30:49 +0800

    Add files via upload

--- src/Sernior.h
+++ src/Sernior.h
@@ -7,45 +7,72 @@
 
 */
 
-typedef void(*fun)();
+
+struct State
+{
+
+    bool faS_workIng;               //阀门不通水false||通水true；
+    bool flowS_workIng;             //流量传感器使用中true
+    TickType_t xTaskGetTickCount_v; //时间戳
+    static int flow1_frequency;            // 水流传感器脉冲频率
+    static int flow2_frequency;            // 水流传感器脉冲频率
+    float Pluse_Different;                  //脉冲差值
+    float flow_1;//传感器1的流量
+    float flow_2;//传感器2的流量
+    float Water_Diffenert; //实际流量差
+    int plusein_pin_1;//脉冲引脚1
+    int plusein_pin_2;//脉冲引脚2
+    int controlPin_1;//控制引脚1
+    int controlPin_2;//控制引脚2
+    char error_message[20];//错误日志
+
+    // Config Con_state;
+    void delete_Task();//挂起任务
+    void change_Fas_State(bool);//阀门不通水false||通水true；
+    static void flew1();
+    static void flew2();
+    float frequencyToWater_h(float flow_frequency);
+    float frequencyToWater_m(float flow_frequency);
+    float frequencyToWater_s(float flow_frequency);
+};
+typedef void(*CB)(State* abc_1);
+
+struct CONFIG
+{
+    int aotoFas_type;                  //设定差类型 0=脉冲差frequency||1=流量差water||2=压力差
+    int aotoFas_value;                 //设定差值(绝对值)
+    TickType_t vTaskDelayUntil_param2; //周期
+    int cpu;                           // 0||1
+
+    CB cb_stop;
+    CB cb_work;
+    CB cb_error;
+    CB cb_message;
+
+    int plusein_pin_1;
+    int plusein_pin_2;
+    int controlPin_1;
+    int controlPin_2;
+
+};
+
+struct StatePtr:State{
+    CONFIG Con_state;
+};
+
+TaskHandle_t Task_con_Hander;
+
+
 typedef class DATA
 {
 public:
-//获取各种传感器数据
-    float GET_flow1();//获取传感器1流量
-    float GET_flow2();//获取传感器2流量
-    float GET_water_diffenert();//h获取传感器差值
-    void  Sernior_init();//初始化硬件
-//控制
-    void close_fa();//手动关闭阀
-    void open_fa();//手动开启阀
-    void Auto_fa(float water_diffenert_target,fun cb);//自动关闭阀，触发：水的流量差达到目标值
-//数据接口
-    void set_water_diffenert_target(float value);//设置目标流量差
-    float GET_water_diffenert_target();//获取目标流量差
-    float GET_flow_1();//传感器1的流量
-    float GET_flow_2();//传感器2的流量
-    float GET_Water_Diffenertex(); //实际流量差
+    DATA(CONFIG Conf_in);
+    void re_struct_task(CONFIG Conf_in);//重建任务
 
 private:
-    float flow_1;//传感器1的流量
-    float flow_2;//传感器2的流量
-    float Water_Diffenert; //实际流量差
-
-    float water_diffenert_target;//目标流量差
-
-    int fa1_state;//阀门1状态 0:关 1：开
-    int fa2_state;//阀门2状态 0:关 1：开
-
+    void init(CONFIG Conf_in);
+    void  Sernior_init(int i,int j,int k,int m);//初始化硬件
+    StatePtr stat_1;
 }Data;
 
-
-//上报数据结构体
-typedef struct DATA_UP
-{
-    float flow_1;//传感器1的流量
-    float flow_2;//传感器2的流量
-    float Water_Diffenert; //实际流量差
-    
-}Data_up;
 
